5166d2764c2ebe9e019804bc2f68a1b8a9a4c565
Reolanta <reolanta@bk.ru>
2025-01-18 16:42:16 +0300

    Change working of create the number
    :qa!
    
    
    c:\Users\reolanta\Desktop\lab-git-project\lab-git-project\src\main.py
    :qa!
    q
    ww
    
    q
    q
    q
    clear
    c:\Users\reolanta\Desktop\lab-git-project\lab-git-project\src\main.py
    
    git log
    '

---
+++ ersreolantaDesktoplab-git-projectlab-git-projectsrcmain.py
@@ -0,0 +1,17 @@
+[33mcommit 3c312cee8e7bfac076131429a1aae52aa3d3f8d6[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mfeature[m[33m)[m
+Author: Reolanta <reolanta@bk.ru>
+Date:   Sat Jan 18 11:42:26 2025 +0300
+
+    Add input
+
+[33mcommit 370ee33d53ad9a0735be2fe8e1c43c796482cfdf[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m, [m[1;32mmain[m[33m)[m
+Author: Reolanta <reolanta@bk.ru>
+Date:   Fri Jan 17 21:27:28 2025 +0300
+
+    Initial project setup
+
+[33mcommit 218177bf341cebd4c7687ec1fde283d702a9938d[m
+Author: Reo225 <reolanta@list.ru>
+Date:   Fri Jan 17 18:37:47 2025 +0300
+
+    Initial commit
